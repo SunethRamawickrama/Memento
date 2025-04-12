7f5e1f458fc4b735a95eeda08bbfbba6dd191cdd
imbulana <damithckumara@gmail.com>
2025-04-12 04:13:49 -0400

    add supabase auth

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,13 +1,50 @@
-'use client';
-export default function Login() {
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+import { createClient } from '@/utils/supabase/component'
+
+export default function LoginPage() {
+  const router = useRouter()
+  const supabase = createClient()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function logIn() {
+    const { error } = await supabase.auth.signInWithPassword({ email, password })
+    if (error) {
+      console.error(error)
+    }
+    router.push('/')
+  }
+
+  async function signUp() {
+    const { error } = await supabase.auth.signUp({ email, password })
+    if (error) {
+      console.error(error)
+    }
+    router.push('/')
+  }
+
   return (
-    <div className="container second-color">
-      <h2>Login</h2>
+    <main>
       <form>
-        <input type="text" placeholder="Username" required /><br />
-        <input type="password" placeholder="Password" required /><br />
-        <button type="submit" className="third-color">Login</button>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={logIn}>
+          Log in
+        </button>
+        <button type="button" onClick={signUp}>
+          Sign up
+        </button>
       </form>
-    </div>
-  );
+    </main>
+  )
 }
